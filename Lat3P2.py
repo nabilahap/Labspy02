67b80525f236c4bdffbd02d43aa1fcb9dbcf1dd4
nabilahap <nabilaananda1325@gmail.com>
2021-11-09 09:32:11 +0700

    Lat3P2.py

--- Lat3P2.py
+++
@@ -1,9 +0,0 @@
-#Latihan3 Tugas Praktikum
-
-a = int(input("Masukkan bilangan A: "))
-b = int(input("Masukkan bilangan B: "))
-c = int(input("Masukkan bilangan C: "))
-if a+b == c or b+c == a or c+a == b:
-    print("BENAR")
-else:
-    print("SALAH")
